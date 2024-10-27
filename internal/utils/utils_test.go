b084ec235b6e9fe55724bb99c22cfd248884b293
P. Taylor Goetz <ptgoetz@gmail.com>
2024-10-27 18:21:41 -0400

    Update README.md and improve test coverage (#7)

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -35,6 +35,40 @@ func TestCountStringOccurrences(t *testing.T) {
 	}
 }
 
+func TestReplaceInFile(t *testing.T) {
+	// Create a temporary directory
+	dir, err := os.MkdirTemp("", "countStringOccurrencesTest")
+	if err != nil {
+		t.Fatalf("Failed to create temp directory: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	// Create a temporary file
+	filePath := filepath.Join(dir, "test.txt")
+	content := "Hello, world!\nHello, Go!\nHello, world!\n"
+	if err := os.WriteFile(filePath, []byte(content), 0644); err != nil {
+		t.Fatalf("Failed to write to test file: %v", err)
+	}
+
+	// Replace "Hello" with "Hi"
+	search := "Hello"
+	replace := "Hi"
+	if err := ReplaceInFile(filePath, search, replace); err != nil {
+		t.Fatalf("ReplaceInFile failed: %v", err)
+	}
+	// Read the file
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Fatalf("Failed to read the file: %v", err)
+	}
+	// Verify the content
+	expectedContent := "Hi, world!\nHi, Go!\nHi, world!\n"
+	if string(fileContent) != expectedContent {
+		t.Errorf("Expected content %q, but got %q", expectedContent, string(fileContent))
+	}
+
+}
+
 // TestGetParentDirAbsolutePath tests the GetParentDirAbsolutePath function
 func TestGetParentDirAbsolutePath(t *testing.T) {
 	// Create a temporary directory
@@ -90,6 +124,26 @@ func TestIsAllAlphabetic(t *testing.T) {
 	}
 }
 
+func TestPaddingString(t *testing.T) {
+	tests := []struct {
+		length   int
+		padChar  string
+		expected string
+	}{
+		{5, "*", "*****"},
+		{0, "*", ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.expected, func(t *testing.T) {
+			actual := PaddingString(test.length, test.padChar)
+			if actual != test.expected {
+				t.Errorf("Expected %s, but got %s", test.expected, actual)
+			}
+		})
+	}
+}
+
 func TestStartsWithDigit(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -105,6 +159,47 @@ func TestStartsWithDigit(t *testing.T) {
 			actual := StartsWithDigit(test.input)
 			if actual != test.expected {
 				t.Errorf("Expected %v, but got %v", test.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIsAllAlphanumeric(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"alpha1", true},
+		{"alpha-", false},
+		{" ", false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			actual := IsAllAlphanumeric(test.input)
+			if actual != test.expected {
+				t.Errorf("Expected %v, but got %v", test.expected, actual)
+			}
+		})
+	}
+}
+
+func TestReplaceInString(t *testing.T) {
+	tests := []struct {
+		input    string
+		old      string
+		new      string
+		expected string
+	}{
+		{"Hello, world!", "world", "Go", "Hello, Go!"},
+		{"Hello, world!", "Go", "world", "Hello, world!"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			actual := ReplaceInString(test.input, test.old, test.new)
+			if actual != test.expected {
+				t.Errorf("Expected %s, but got %s", test.expected, actual)
 			}
 		})
 	}
